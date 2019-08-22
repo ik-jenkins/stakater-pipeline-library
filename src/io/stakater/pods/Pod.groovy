659ea65a07d7c9979be668dad5dd4bb2b6fb547a
Usama Ahmad <usama.ahmad.khan@hotmail.com>
2019-08-22 11:52:18 +0500

    fix array search for envVars

--- src/io/stakater/pods/Pod.groovy
+++ src/io/stakater/pods/Pod.groovy
@@ -44,7 +44,7 @@ def enableChartMuseum(Map parameters = [:]) {
 def setDefaultContainerEnvVarsConfig(Map parameters) {
     setDefaultContainer(parameters)
 
-    if ( ! parameters.podContainers.defaultContainer.grep('envVarsConfig') ) {
+    if ( ! parameters.podContainers.defaultContainer.contains('envVarsConfig') ) {
         parameters.podContainers.defaultContainer.envVarsConfig = [:]
     }
 }
