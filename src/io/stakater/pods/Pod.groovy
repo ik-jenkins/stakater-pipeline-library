51a43be274ad210120cae85c292bbf9c6a5771ee
Usama Ahmad <usama.ahmad.khan@hotmail.com>
2019-08-22 11:23:07 +0500

    fix envVars default value

--- src/io/stakater/pods/Pod.groovy
+++ src/io/stakater/pods/Pod.groovy
@@ -44,7 +44,7 @@ def enableChartMuseum(Map parameters = [:]) {
 def setDefaultContainerEnvVarsConfig(Map parameters) {
     setDefaultContainer(parameters)
 
-    if ( ! parameters.podContainers.defaultContainer.get('envVarsConfig', false) ) {
+    if ( ! parameters.podContainers.defaultContainer.get('envVarsConfig', [:]) ) {
         parameters.podContainers.defaultContainer.envVarsConfig = [:]
     }
 }
